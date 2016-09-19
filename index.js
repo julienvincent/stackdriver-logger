a7e8d07d8fb9be2b3b9033c4cb46112eda75c2f6
Julien Vincent <julienlucvincent@gmail.com>
2016-09-19 17:58:55 +0200

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+// @flow
+import io from 'socket.io-client'
+import _ from 'lodash'
+
+type ConnectionInfoType = {
+    url: String,
+    key: String,
+    pod: String,
+    kind: String
+}
+
+type Level = "INFO" | "WARNING" | "ERROR"
+
+type SocketIO = {
+    on: Function,
+    emit: Function
+}
+
+const createLogger = (ConnectionInfo: ConnectionInfoType) => {
+    const socket: SocketIO = io(ConnectionInfo.url)
+
+    let connected = false
+    const backlog = []
+
+    socket.on('connect', () => {
+        socket.emit('register', {
+            key: ConnectionInfo.key,
+            kind: ConnectionInfo.kind,
+            pod: ConnectionInfo.pod
+        })
+    })
+
+    socket.on('accepted', () => {
+        connected = true
+
+        _.forEach(backlog, (log, i) => {
+            socket.emit('log', log)
+            backlog.splice(i, 1)
+        })
+    })
+
+    const logger = (message: String, payload: ?any, level: ?Level) => {
+        const log = {
+            message,
+            payload: payload || null,
+            level: level || "INFO"
+        }
+
+        if (connected) {
+            socket.emit('log', log)
+        } else {
+            backlog.push(log)
+        }
+    }
+
+    logger.error = (message, payload) => {
+        logger(message, payload, "ERROR")
+    }
+
+    logger.warn = (message, payload) => {
+        logger(message, payload, "WARNING")
+    }
+
+    return logger
+}
+
+export { createLogger as default, createLogger }
